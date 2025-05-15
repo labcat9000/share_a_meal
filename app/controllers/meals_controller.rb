1337f3d6cfae6efec671e7aef842d8580511598f
henrique vera <hverar@gmail.com>
2025-05-15 19:26:23 -0400

    modifications

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -2,4 +2,11 @@ class MealsController < ApplicationController
   def index
     @meals = Meal.all
   end
+
+  def show
+    @meal = Meal.find_by(id: params[:id])
+    if @meal.nil?
+      redirect_to meals_path, alert: "Meal not found."
+    end
+  end
 end
