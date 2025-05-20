dee966d888a8266329e025ee65787d9609c55230
henrique vera <hverar@gmail.com>
2025-05-20 18:26:31 -0400

    FINALLY FIXED

---
+++ app/javascript/controllers/index.js
@@ -0,0 +1,5 @@
+// Import and register all your controllers from the importmap via controllers/**/*_controller
+import { application } from "controllers/application"
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+eagerLoadControllersFrom("controllers", application)
+// Henrique, please add the original content on this file (i.e. lines 1-4 above)
--- app/javascript/controllers/map_index_controller.js
+++ app/javascript/controllers/map_index_controller.js
@@ -1,17 +1,31 @@
 import { Controller } from "@hotwired/stimulus"
-import mapboxgl from 'mapbox-gl' // Don't forget this!
+import mapboxgl from 'mapbox-gl'
 
 export default class extends Controller {
-  static values = {
-    apiKey: String,
-  }
+  static values = { apiKey: String }
+  static targets = ["map"]
 
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
 
     this.map = new mapboxgl.Map({
-      container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      container: this.mapTarget,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [-73.5673, 45.5019],
+      zoom: 13
     })
+
+    this.map.addControl(new mapboxgl.NavigationControl(), 'top-left')
+  }
+
+  locatePosition() {
+    navigator.geolocation.getCurrentPosition(position => {
+    const lng = position.coords.longitude;
+    const lat = position.coords.latitude;
+    this.map.flyTo({ center: [lng, lat], zoom: 14 });
+    new mapboxgl.Marker({ color: "#d00" })
+      .setLngLat([lng, lat])
+      .addTo(this.map);
+  })
   }
 }
