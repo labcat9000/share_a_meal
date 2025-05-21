f973a84c68bd4f0c9700373acd99700b5bd3d5b2
henrique vera <hverar@gmail.com>
2025-05-20 20:09:56 -0400

    done with default photos in the index and show page

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,6 +2,7 @@ class Meal < ApplicationRecord
   belongs_to :user, optional: true
   validates :name, presence: true
   validates :description, presence: true
+  has_one_attached :photo
 
   def average_rating
     ratings.average(:value)&.round(1) || "No ratings yet"
