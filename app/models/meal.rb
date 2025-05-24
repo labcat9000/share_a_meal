696a64ad15067a3c110b712e9770aec3d91111f1
henrique vera <hverar@gmail.com>
2025-05-24 15:13:40 -0400

    buttons fixed

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,4 +1,13 @@
 class Meal < ApplicationRecord
+  CATEGORY_OPTIONS = [
+    'Breakfast',
+    'Lunch',
+    'Snack',
+    'Side',
+    'Dinner',
+    'Dessert'
+  ].freeze
+
   belongs_to :user, optional: true
   has_many :exchanges
 
@@ -6,9 +15,10 @@ class Meal < ApplicationRecord
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
-  
+
   validates :name, presence: true
   validates :description, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORY_OPTIONS }
 
   scope :available, -> {
     where.not(id: Exchange.select(:meal_offered_id))
