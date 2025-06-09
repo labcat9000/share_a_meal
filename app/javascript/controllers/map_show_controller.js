c9fab51022193ce48074b027e9fed04331c72a3a
henrique vera <hverar@gmail.com>
2025-06-09 15:05:01 -0400

    map is almsot done colors are done

--- app/javascript/controllers/map_show_controller.js
+++ app/javascript/controllers/map_show_controller.js
@@ -5,23 +5,57 @@ export default class extends Controller {
   static values = {
     apiKey: String,
     latitude: Number,
-    longitude: Number
+    longitude: Number,
+    status: String
   }
 
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
 
+    const center = [this.longitudeValue, this.latitudeValue]
+
     this.map = new mapboxgl.Map({
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v11",
-      center: [this.longitudeValue, this.latitudeValue],
+      center,
       zoom: 13
     })
 
-    new mapboxgl.Marker()
-      .setLngLat([this.longitudeValue, this.latitudeValue])
-      .addTo(this.map)
-
     this.map.addControl(new mapboxgl.NavigationControl(), "top-left")
+
+    this.map.on("load", () => {
+      if (this.statusValue === "accepted") {
+        new mapboxgl.Marker()
+          .setLngLat(center)
+          .addTo(this.map)
+      } else {
+        this.map.addSource("blurred-meal", {
+          type: "geojson",
+          data: {
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: center
+            }
+          }
+        })
+
+        this.map.addLayer({
+          id: "blurred-circle",
+          type: "circle",
+          source: "blurred-meal",
+          paint: {
+            "circle-radius": {
+              stops: [[0, 0], [20, 5000]],
+              base: 2
+            },
+            "circle-color": "#88f065",
+            "circle-opacity": 0.15,
+            "circle-stroke-color": "#88f065",
+            "circle-stroke-width": 1
+          }
+        })
+      }
+    })
   }
 }
